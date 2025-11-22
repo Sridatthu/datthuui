e7ee562ef27e646aabe6b6f9e17d3293cbbea580
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-11-22 12:18:45 +0530

    feat: initialize new web application with component library, documentation, and blog features.

---
+++ apps/www/config/docs.ts
@@ -0,0 +1,42 @@
+import { NavItem, NavItemWithChildren } from "@/types"
+
+interface DocsConfig {
+  mainNav: NavItem[]
+  sidebarNav: NavItemWithChildren[]
+}
+
+export const docsConfig: DocsConfig = {
+  mainNav: [
+    {
+      title: "Components",
+      href: "/components",
+    },
+    {
+      title: "Templates",
+      href: "https://pro.datthuui.site",
+      event: "header_cta_clicked",
+      label: "",
+    },
+    {
+      title: "Showcase",
+      href: "/showcase",
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs",
+          items: [],
+        },
+        {
+          title: "Installation",
+          href: "/docs/installation",
+          items: [],
+        },
+      ],
+    },
+  ],
+}
---
+++ apps/www/config/site.ts
@@ -0,0 +1,23 @@
+export const siteConfig = {
+  name: "Datthu UI",
+  url: "https://datthuui.site",
+  ogImage: "https://datthuui.site/og",
+  description:
+    "Beautifully designed landing page components built with React & Tailwind CSS.",
+  links: {
+    twitter: "https://twitter.com/483Sri",
+    discord: "",
+    github: "https://github.com/",
+    instagram: "https://instagram.com/",
+  },
+  keywords: [
+    "React",
+    "Tailwind CSS",
+    "Motion",
+    "Landing Page",
+    "Components",
+    "Next.js",
+  ],
+}
+
+export type SiteConfig = typeof siteConfig
