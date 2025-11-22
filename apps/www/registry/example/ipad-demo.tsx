f7fdc0c7bce4e0d1aa72a268de7276d92a68038b
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-11-22 19:09:31 +0530

    updated images

--- apps/www/registry/example/ipad-demo.tsx
+++ apps/www/registry/example/ipad-demo.tsx
@@ -4,7 +4,7 @@ export function iPadDemo() {
   return (
     <div className="relative">
       <Ipad
-        src="https://res.cloudinary.com/eldoraui/image/upload/v1758616622/ipad_xgn1qu.jpg"
+        src="https://images.unsplash.com/photo-1733249152111-cde0234c5fed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aXBhZCUyMHdhbGxwYXBlciUyMGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D"
         className="size-full"
       />
     </div>
--- apps/www/registry/example/safari-browser-demo.tsx
+++ apps/www/registry/example/safari-browser-demo.tsx
@@ -5,7 +5,7 @@ export function SafariBrowserDemo() {
     <div className="relative">
       <Safari
         url="datthui.vercel.app"
-        src="https://res.cloudinary.com/eldoraui/image/upload/v1758570676/mac_llxzg3.jpg"
+        src="https://images.unsplash.com/photo-1763063462165-94125cccf210?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D"
         className="size-full"
       />
     </div>
