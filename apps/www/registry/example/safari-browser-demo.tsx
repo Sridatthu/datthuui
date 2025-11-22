118c7feebab66b967b67d77b5da01526d35889d8
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-11-22 16:21:04 +0530

    added safari browser component

---
+++ apps/www/registry/example/safari-browser-demo.tsx
@@ -0,0 +1,13 @@
+import { Safari } from "@/registry/datthuui/safari-browser"
+
+export function SafariBrowserDemo() {
+  return (
+    <div className="relative">
+      <Safari
+        url="datthui.vercel.app"
+        src="https://res.cloudinary.com/eldoraui/image/upload/v1758570676/mac_llxzg3.jpg"
+        className="size-full"
+      />
+    </div>
+  )
+}
